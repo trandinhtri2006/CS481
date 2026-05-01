0a7be02463cd58b0873d23896ab37982a4596e38
Tony Tran <trandinhtri20106@gmail.com>
2026-05-01 02:45:16 -0700

    Testing Android Studio Git

--- app/src/main/java/com/example/cs481app/ui/scenes/ChangeEmailPage.kt
+++ app/src/main/java/com/example/cs481app/ui/scenes/ChangeEmailPage.kt
@@ -1,2 +1,18 @@
 package com.example.cs481app.ui.scenes
 
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+
+class ChangeEmailViewModel : ViewModel() {
+
+}
+
+@Composable
+fun ChangeEmail(
+    navController: NavController,
+    viewModel: ChangeEmailViewModel = viewModel()
+) {
+
+}
